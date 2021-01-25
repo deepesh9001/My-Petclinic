6f5a9487f21de407b2aa790d834ffc0ccc637f45
deepesh9001 <dipubkn14@gmail.com>
2021-01-25 14:17:23 +0530

    added base entity. closes #9

--- pet-clinic-data/src/main/java/com/deepesh/mypetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/deepesh/mypetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.deepesh.mypetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
